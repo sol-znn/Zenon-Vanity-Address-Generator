5836b21f0b96ec7ef29b141792940349f92b3c0b
Sol Sanctum <so1_sanctum@pm.me>
2022-06-02 21:12:42 -0400

    Closes #1; some optimizations

--- vanity.dart
+++ vanity.dart
@@ -4,41 +4,31 @@
 
 import 'package:hex/hex.dart';
 import 'package:znn_sdk_dart/znn_sdk_dart.dart';
-import 'package:bip39/bip39.dart' as bip39;
 
 Future<void> main() async {
   //Example: this will try to find an address like z1qr3uww8uqh75qnsuxqajegvwaesqynfglr1337
   var criteria = '1337'; // <--- update this
   var verbose = true;
 
-  var add = 'z1qr3uww8uqh75qnsuxqajegvwaesqynfglrare2';
-  var valid = false;
-  var mnemonic = '';
   var match = false;
   var count = 0;
 
   while (!match) {
-    while (!valid) {
-      var mnemonic1 = bip39.generateMnemonic();
-      var mnemonic2 = bip39.generateMnemonic();
-      mnemonic = mnemonic1 + " " + mnemonic2; //please don't laugh at me
-      valid = bip39.validateMnemonic(mnemonic);
-    }
-
-    var keyStore = KeyStore.fromMnemonic(mnemonic);
+    var keyStore = await KeyStore.newRandom();
+    var mnemonic = keyStore.mnemonic;
     var keyPair = keyStore.getKeyPair(0);
     var privateKey = keyPair.getPrivateKey();
     var publicKey = await keyPair.getPublicKey();
     var address = await keyPair.address;
+    var addressLength = address.toString().length;
 
     var str = address.toString();
-    var target = str.substring(add.length - criteria.length, add.length);
+    var target = str.substring(addressLength - criteria.length, addressLength);
 
     if (verbose) { print("${count}: ${str} || ${mnemonic}"); }
 
     if (target != criteria) {
       count++;
-      valid = false;
     }
     else {
       print('mnemonic: ${mnemonic}');
